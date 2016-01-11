2ab21db3917e1728a8d78824d63d422e48991f76
akinomaeni <akinomaeni@gmail.com>
2016-01-12 06:43:42 +0900

    04

---
+++ math_puzzle/04.rb
@@ -0,0 +1,28 @@
+require 'benchmark'
+require 'pry-byebug'
+
+puts "file: " + File.basename(__FILE__)
+puts ""
+
+result = Benchmark.measure do
+  ## 
+
+  def count(n, m)
+    c = 0
+
+    while(n > 0)
+      l = [m, 2**c].min
+      n -= l
+      c += 1
+    end
+
+    c
+  end
+
+  puts count(20, 3)
+  puts count(100, 5)
+end
+
+puts ""
+puts Benchmark::CAPTION
+puts result
